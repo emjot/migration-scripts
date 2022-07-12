40d7feb99adbe54b8f51bf00545793b4791d45d0
msalzburg <salzburg@emjot.de>
2022-07-12 16:43:52 +0200

    Add custom pluralize config for "global-metadata"

--- v3-sql-v4-sql/index.js
+++ v3-sql-v4-sql/index.js
@@ -1,8 +1,11 @@
 require("dotenv").config();
 
+const pluralize = require('pluralize')
+
 const { migrate } = require("./migrate");
 
 async function f() {
+  pluralize.addUncountableRule('global-metadata') // configure pluralize analog to cms project
   await migrate();
 
   process.exit();
